8977b63bcb781e7ffbde06f769de60b220636385
jpetterson <jpetterson@DS-17.decker.local>
2018-02-07 14:25:03 -0600

    2-7-18

---
+++ src/Char_Arrays_Exercise.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+// Name        : Char_Arrays_Exercise.cpp
+// Author      : 
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+
+/****************************************************************
+ * 	1-create a char string and output it.			*
+ * 	2-output all the chars with a for loop			*
+ * 	3-output all the chars with a 'while(true)' loop	*
+ * 								*
+ * **************************************************************/
+
+int main() {
+	char test[] = "Make It Now!";
+
+	cout << test << endl;
+
+
+	return 0;
+}
